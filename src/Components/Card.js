92073324e88c73fa12af2b51575a8b039d2fb152
Krystle Lau <krystlelau90@gmail.com>
2022-05-16 16:05:57 +0800

    added modal

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,10 @@
-import React from "react";
-import bookcoversample from "../images/bookcover_sample.jpg";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Card = ({ book }) => {
-  console.log("fromcards", book);
+  const [show, setShow] = useState(false);
+  const [bookItem, setBookItem] = useState("");
+
   return (
     <>
       {book.map((item) => {
@@ -16,13 +18,20 @@ const Card = ({ book }) => {
         if (thumbnail !== undefined) {
           return (
             <>
-              <div className="card">
+              <div
+                className="card"
+                onClick={() => {
+                  setShow(true);
+                  setBookItem(item);
+                }}
+              >
                 <img src={thumbnail} alt="book cover sample" />
                 <div className="bottom">
                   <h3 className="title">{bookTitle}</h3>
                   <h3 className="author">{bookAuthor}</h3>
                 </div>
               </div>
+              <Modal show={show} bookItem={bookItem} />
             </>
           );
         }
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,23 @@
 import React from "react";
 import bookcoversample from "../images/bookcover_sample.jpg";
 
-const Modal = () => {
+const Modal = ({ bookItem, show }) => {
+  if (!show) {
+    return null;
+  }
+  let thumbnail =
+    bookItem.volumeInfo.imageLinks &&
+    bookItem.volumeInfo.imageLinks.smallThumbnail;
+
+  let bookTitle = bookItem.volumeInfo.title;
+
+  let bookAuthor = bookItem.volumeInfo.authors;
+
+  let bookPublisher = bookItem.volumeInfo.publisher;
+
+  let bookPublisherYear = bookItem.volumeInfo.publishedDate;
+
+  let bookDescription = bookItem.volumeInfo.description;
   return (
     <>
       <div className="overlay">
@@ -10,8 +26,16 @@ const Modal = () => {
             <i class="fas fa-times"></i>
           </button>
           <div className="inner-box">
-            <img src={bookcoversample} alt="bookcoversample" />
+            <img src={thumbnail} alt="" />
+            <div className="info">
+              <h1>{bookTitle}</h1>
+              <h3>{bookAuthor}</h3>
+              <h4>{bookPublisher}</h4>
+              <h4>{bookPublisherYear}</h4>
+              <br />
+            </div>
           </div>
+          <h4 className="description">{bookDescription}</h4>
         </div>
       </div>
     </>
